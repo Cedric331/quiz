ae051156114848be5aa3461e4c2828fac7195d8e
Cedric Lima <limacedric@hotmail.fr>
2021-02-11 23:16:53 +0100

    Installation de inertia et configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 require('./bootstrap');
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
+
+import { App, plugin } from '@inertiajs/inertia-vue'
+import Vue from 'vue'
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init()
+
+Vue.use(plugin)
+
+const el = document.getElementById('app')
+
+
+new Vue({
+  render: h => h(App, {
+    props: {
+      initialPage: JSON.parse(el.dataset.page),
+      resolveComponent: name => require(`./Pages/${name}`).default,
+    },
+  }),
+}).$mount(el)
