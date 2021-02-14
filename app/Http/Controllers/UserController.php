34b309d024b143ef982f239b1fc42cae500f08b1
Cedric Lima <limacedric@hotmail.fr>
2021-02-14 17:55:08 +0100

    Ajout function update et delete User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -12,6 +14,28 @@ class UserController extends Controller
    {
       return Inertia::render('Account', [
          'show-quiz' => URL::route('show-quiz'),
+         'quiz' => URL::route('quiz-index')
+     ]);
+   }
+
+   public function update(Request $request)
+   {
+      $request->validate([
+         'name' => ['required', 'string', 'max:255'],
+         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+      ]);
+
+      User::where('id', Auth::user()->id)
+            ->update(['name' => $request->name,'email' => $request->email]);
+
+   }
+
+   public function delete()
+   {
+      User::findOrFail(Auth::user()->id)->delete();
+
+      return Inertia::render('Home', [
+         'url' => URL::route('login'),
          'quiz' => URL::route('quiz-index')
      ]);
    }
